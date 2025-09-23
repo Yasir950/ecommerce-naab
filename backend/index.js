2fb3d5e8165ff453ac22625ca69b5afc92e420f6
Muhammad Yasir Abbas <368yasir@gmail.com>
2025-09-23 19:48:48 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
-
+dotenv.config();
 app.get('/', (req, res)=>{
     res.send('hello it beginning');
 })
+const PORT = process.env.PORT || 8000;
 app.listen(8000, ()=>{
     console.log('listening on port 8000')
 })

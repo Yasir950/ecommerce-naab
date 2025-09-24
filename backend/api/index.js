b7858a98588aab26849d1476f3255c0e96021fa8
Muhammad Yasir Abbas <368yasir@gmail.com>
2025-09-24 14:31:41 +0500

    changes

---
+++ backend/api/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import router from '../router/userRouter.js';
+const app = express();
+dotenv.config();
+const MONGOURL = process.env.MONGO_URL
+const PORT = process.env.PORT
+mongoose.connect(MONGOURL).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch(err=>console.log(err))
+
+app.use('/', router);

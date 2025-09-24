75d6d847436f217af5cf2749ec776d4bd233f92e
Muhammad Yasir Abbas <368yasir@gmail.com>
2025-09-24 14:00:04 +0500

    changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,20 @@
 import express from "express";
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app = express();
-
+dotenv.config();
+const MONGOURL = process.env.MONGO_URL;
+const PORT = process.env.PORT;
+console.log(MONGOURL)
+console.log(PORT)
+mongoose.connect(MONGOURL).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express on Vercel!" });
+//   res.json({ message: "Hello from Express on Vercel!" });
 });
 
 export default app;

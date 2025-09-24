b9336bed876a1584de85aba208637f9a330992a1
Muhammad Yasir Abbas <368yasir@gmail.com>
2025-09-24 14:49:58 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,11 @@ const MONGOURL = process.env.MONGO_URL
 const PORT = process.env.PORT
 mongoose.connect(MONGOURL).then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //     console.log(`Server is running on port ${PORT}`);
+    // });
 }).catch(err=>console.log(err))
 
 app.use('/', router);
+
+export default app;
